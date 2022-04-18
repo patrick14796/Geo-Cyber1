f2b2207be326e051a72e5d094cb4d84bbc5d7919
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2022-04-18 20:58:27 +0300

    Upload Telecome sites with data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "leaflet/dist/leaflet.css"
 import './App.css'
 
-import {MapContainer, TileLayer, GeoJSON} from 'react-leaflet'
+
+
+import {MapContainer, TileLayer, GeoJSON,Marker, Popup} from 'react-leaflet'
 import mapProvider from "./mapProvider";
 import israelGeoJson from './layers/Israel Marine Cables.json'
+import telecomTerminals from './layers/TelecomTerminals2018.json.json'
+import React, { Component } from 'react';
+import L from 'leaflet';
+
+
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const center = [32.109333, 34.85549]
 
@@ -23,6 +40,23 @@ function App() {
         layer.bindTooltip(cableData, {sticky: true})
     }
 
+
+	const onEachIsraeliLandingSite = (terminal, layer) => {
+		const terminalName = terminal.properties.Name
+		const CabelsConnected2Terminal = terminal.properties.cable_id
+		const CityID = terminal.properties.city_id
+		const TerminalType = terminal.geometry.type
+		const Fixcord = terminal.geometry.coordinates
+		const terminalData = `Terminal name: ${terminalName}<br/>
+		Cables Connected: ${CabelsConnected2Terminal}<br/>
+		City ID: ${CityID}<br/>
+		Type: ${TerminalType}<br/>
+		Coordinate: ${Fixcord}`
+		layer.bindTooltip(terminalData, {sticky: true})
+	}
+
+
+
     return (
         <MapContainer id="map" center={center} zoom={9}>
             <TileLayer
@@ -30,6 +64,7 @@ function App() {
                 attribution={mapProvider.attribution}
             />
             <GeoJSON data={israelGeoJson} onEachFeature={onEachIsraeliCable} />
+			<GeoJSON data={telecomTerminals} onEachFeature={onEachIsraeliLandingSite} />
         </MapContainer>
     );
 }
